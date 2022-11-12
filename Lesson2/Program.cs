e126013aa2606968fae4e535c7103339c0c135a3
Chronik <vladislamakarevich@mail.ru>
2022-11-12 13:37:00 +0300

    gt

--- Lesson2/Program.cs
+++ Lesson2/Program.cs
@@ -345,9 +345,12 @@ int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
     int[,] array = new int[rows,columns];
 
     for(int i = 0; i < rows; i++)
+    {
+
         for(int j = 0; j < columns; j++)
             array[i,j] = new Random().Next(minValue, maxValue + 1);
-
+            
+    }
     return array;
 }
  void Show2dArray(int[,] array)
